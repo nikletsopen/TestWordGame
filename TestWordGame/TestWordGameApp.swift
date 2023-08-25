5d433cb17c8255514009ee0ad32545edf8a84e12
Nikita Timonin <nikita@letsopen.co>
2023-08-25 17:12:52 +0300

    Initial Commit

---
+++ TestWordGame/TestWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWordGameApp.swift
+//  TestWordGame
+//
+//  Created by Nikita Timonin on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
